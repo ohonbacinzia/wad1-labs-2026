4e40f7f8fb059b408a7cadef068f3fb6c4aa1a2c
Cinzia-cici <20112720@mail.wit.ie>
2026-02-13 10:54:02 +0000

    “Update”

--- controllers/start.js
+++ controllers/start.js
@@ -3,7 +3,7 @@ import logger from "../utils/logger.js";
 const start = {
   createView(request, response) {
     logger.info("Start page loading!");
-    response.send('Welcome to the Playlist app!');   
+    response.send('Welcome to Cinzia Playlist app!');   
   },
 };
 
